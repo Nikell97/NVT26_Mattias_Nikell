dd5102924106d75c57b4fb457cd32c870f74d98c
Mattias N <nikellmattias@gmail.com>
2026-09-10 13:57:16 +0200

    added input to ipaddress for better reusability

--- vecka-03/subnet.py
+++ vecka-03/subnet.py
@@ -1,6 +1,8 @@
 import ipaddress
 
-text = "192.168.1.0/26"
+# text = "192.168.1.64/26"
+print("Please enter ip address, ex. 192.168.1.64/26: ")
+text = input()
 
 net = ipaddress.ip_network(text, strict=False)
 
